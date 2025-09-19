2a18100f4661936ecad629d606e27259d0141484
fadihaddad <fadi_nwvg@live.nl>
2025-09-19 17:17:09 +0200

    Initial commit

---
+++ tests/GeneralLinksTest.spec.js
@@ -0,0 +1,69 @@
+import { test, expect } from '@playwright/test';
+
+
+//Playwright by default runs in headless mode
+//To run tests, run the following in terminal: npx playwright test 
+//To run tests in head-mode, run the following in terminal: npx playwright test --headed
+//Or in config file place >>> headless : false
+//test.only to run a specific test in isolation 
+
+
+//Testing the "WAT BIEDEN WIJ" link on the homepage 
+test('WAT BIEDEN WIJ-test', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("li").getByText("Wat bieden wij").click();
+    await expect(page.getByText('De specialisten van UnameIT')).toBeVisible();  
+});
+
+//Testing the "WHATSAPP MET LEF" link on the homepage 
+test('WHATSAPP MET LEF-test', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='WhatsApp met LEF'").click();
+    await expect(page.getByText('Stel je voor')).toBeVisible();  
+}); 
+
+//Testing the "ALLE OPLOSSINGEN" link on the homepage 
+test('ALLE OPLOSSINGEN', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='Alle oplossingen'").first().click();
+    await expect(page.getByText('Websites').first()).toBeVisible();  
+}); 
+
+//Testing the "ALLE OPLOSSINGEN" link on the homepage 
+test('WIE ZIJN WIJ', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='Wie zijn wij'").click();
+    await expect(page.getByText('Wij helpen autobedrijven succesvol te zijn')).toBeVisible();  
+}); 
+
+//Testing the "NIEUWS" link on the homepage 
+test('NIEUWS', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='Nieuws'").first().click();
+    await expect(page.locator('.paragraph__title')).toContainText('Nieuws');
+}); 
+
+//Testing the "CONTACT" link on the homepage 
+test('CONTACT', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='Contact'").first().click();
+    await expect(page.getByText('Adresgegevens')).toBeVisible(); 
+}); 
+
+//Testing the "DEMO?" link on the homepage 
+test('DEMO?', async ({page})=> 
+{
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='Demo?'").click();
+await expect(page.locator('.paragraph__title').first()).toContainText('Vraag een demo aan');
+}); 
+
+
+
+
---
+++ tests/LeasemoduleTest.spec.js
@@ -0,0 +1,85 @@
+import { test, expect } from '@playwright/test';
+
+//Testing the Lease module solution 
+test('Lease module solution ', async ({page})=> 
+{
+    //navigates to the lease module
+    await page.goto("https://www.uname-it.com/");
+    await page.locator("text='Alle oplossingen'").first().click();
+    await page.locator('.dropdown-menu >> text=Websites').first().click();
+    await page.locator(".item-title >> text='UnameIT Lease module'").click();
+    await expect(page.locator('.page__title')).toContainText('Bekijk de vernieuwde Online leasemodule!'); 
+    
+    //selects 50.000 km as milage 
+    await page.locator('#filter-km').nth(1).selectOption('50000');
+
+    //selects 48 months as runtime
+    await page.locator('#filter-duration').nth(1).selectOption('48');
+
+    //selects BMW as the car brand
+    await page.getByText('BMW').nth(1).click();
+
+   //selects benzine as fuel
+    await page.locator('text="Brandstof"').nth(1).click();
+    await page.getByText('Benzine (1)').last().click();
+
+    //select the available lease car
+    await page.locator('a div.btn').click();
+
+    //select the available lease car
+    await page.locator('a.btn-primary').click();
+    await page.getByText('Bestel deze auto ').click();
+
+    //fill the form with the required info 
+    await page.getByText('Dhr.').click();
+    await page.locator('#firstname').fill('Fadi');
+    await page.locator('#lastname').fill('Haddad');
+    await page.locator('#email').fill('fadi_nwvg@live.nl');
+    await page.locator('#phone').fill('0643597472');
+    await page.locator('#street').fill('mijnstraat');
+    await page.locator('#housenumber').fill('123');
+    await page.locator('#postalcode').fill('1234AB');
+    await page.locator('#city').fill('Amsterdam');
+    await page.getByText('Ik heb de privacyverklaring gelezen').click();
+    await page.getByText('Houd mij op de hoogte van nieuws en aanbiedingen').click();
+    await page.getByText('Bestelling afronden').last().click();
+
+    //Assertion to check if all went well
+    await expect(page.locator('.view-confirmation')).toContainText('Bedankt voor uw aanvraag!'); 
+
+
+    //await page.pause();
+    
+
+
+
+
+
+    
+    
+  
+    
+
+    
+
+
+
+    
+
+    
+
+    
+    
+
+
+
+
+
+});
+
+
+   
+
+
+
+   

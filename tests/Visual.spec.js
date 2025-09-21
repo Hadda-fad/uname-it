fee854a58bff571d57db421e6376eb41d5d8b7f8
fadihaddad <fadi_nwvg@live.nl>
2025-09-21 13:25:44 +0200

    ++

--- tests/Visual.spec.js
+++ tests/Visual.spec.js
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-// Visual comparison test of the navigation bar on the homepage 
+// Visual comparison test of the navigation bar on the homepage/validating alignment 
 test.only('Visual Comparison', async ({ page }) => {
   await page.goto("https://www.uname-it.com/");
   await expect(page).toHaveTitle(/UnameIT | Automotive Software Company | 25+ Jaar Ervaring/);
   await page.locator('.region--navigation').screenshot({ path: 'navigation_screenshot.png' });
+
+  expect (await page.locator('.region--navigation').screenshot()).toMatchSnapshot('landing.png');
+
   
 
  
